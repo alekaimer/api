15b1e646769192a2cbd9a78029b05863d575f0b3
Alexandre Kaimer <alekaimer@gmail.com>
2021-02-03 11:17:57 -0300

    buscando response no back-end com axios

--- server.js
+++ server.js
@@ -4,12 +4,25 @@ const cors = require('cors')
 const express = require('express')
 const app = express()
 
+const axios = require('axios')
+
 app.use(cors())
 
-app.get('/', (req, res)=>res.json(
-  [
-    {name: 'Alexandre'},
-    {name: 'Ariane'},
-  ]
-))
+//front-end
+// app.get('/', async (req, res)=>{
+//   return res.json(
+//     [
+//       {name: 'Alexandre'},
+//       {name: 'Ariane'},
+
+//    ]
+//   )
+// })
+
+//back-end
+app.get('/', async (req, res)=>{
+  const {data} = await axios('https://jsonplaceholder.typicode.com/users') // pegando response.data de dentro da response do axios com desestruturação
+  return res.json(data)
+})
+
 app.listen('4567')
