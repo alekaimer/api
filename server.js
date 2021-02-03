4ff4ac77e9930773b18ca353359a523670e9cdab
Alexandre Kaimer <alekaimer@gmail.com>
2021-02-03 09:38:14 -0300

    refatora server

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 
+const cors = require('cors')
+
 const express = require('express')
 const app = express()
 
-const cors = require('cors')
-
-app.get('/', (req, res)=>res.json([{message: 'OK'}]))
-app.listen('4567')
-
 app.use(cors())
+
+app.get('/', (req, res)=>res.json(
+  [
+    {user: 'Alexandre'},
+    {user: 'Ariane'},
+  ]
+))
+app.listen('4567')
